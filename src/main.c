c48ee110a58e04767fa272267e2f020100f6364b
Guy John <guy@rumblesan.com>
2012-08-27 10:59:02 +0100

    Adding zooming functionality
    Needs to be made more accurate, update all floats to use doubles, and
    have some fixes to maintain aspect ratio

--- src/main.c
+++ src/main.c
@@ -31,6 +31,9 @@ void draw_screen(Mandelbrot brot, SDL_Surface* screen)
 {
     int xPos, yPos, colour;
 
+    printf("drawn: %f,%f   to   %f,%f\n", brot->x1, brot->y1, brot->x2, brot->y2);
+
+
     if(SDL_MUSTLOCK(screen)) {
         if(SDL_LockSurface(screen) < 0) {
             return;
@@ -49,6 +52,46 @@ void draw_screen(Mandelbrot brot, SDL_Surface* screen)
     }
 
     SDL_Flip(screen);
+
+}
+
+/* Given pixel coordinates, this will render a selected section of
+ * the screen after zooming in
+ */
+void draw_mandelbrot_section(Mandelbrot brot, int x1, int y1, int x2, int y2)
+{
+    int temp;
+
+    if (x1 > x2) {
+        temp = x2;
+        x2 = x1;
+        x1 = temp;
+    }
+
+    if (y1 < y2) {
+        temp = y2;
+        y2 = y1;
+        y1 = temp;
+    }
+
+    printf("pixel: %i,%i   to   %i,%i\n", x1, y1, x2, y2);
+
+    float x1Brot, y1Brot, x2Brot, y2Brot;
+
+    float plotX = (brot->x2 - brot->x1);
+    float plotY = (brot->y2 - brot->y1);
+
+    x1Brot = (plotX * ((float)x1 / brot->pixelWidth)) + brot->x1;
+    y1Brot = brot->y2 - (plotY * ((float)y1 / brot->pixelHeight));
+
+    x2Brot = (plotX * ((float)x2 / brot->pixelWidth)) + brot->x1;
+    y2Brot = brot->y2 - (plotY * ((float)y2 / brot->pixelHeight));
+
+    printf("calced: %f,%f   to   %f,%f\n", x1Brot, y1Brot, x2Brot, y2Brot);
+
+    brot_recreate(brot, x1Brot, y1Brot, x2Brot, y2Brot);
+
+    brot_calculate(brot);
 
 }
 
@@ -93,6 +136,8 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    int x1, x2, y1, y2;
+
     Mandelbrot brot = brot_create(WIDTH, HEIGHT, 255, -2.5, -1.0, 1.0, 1.0);
 
     brot_calculate(brot);
@@ -118,16 +163,28 @@ int main(int argc, char* argv[])
                     // Write out png
                     render_png(brot);
                     break;
+                case SDLK_r:
+                    // Reset image
+                    // This needs to be Non height/width specific
+                    printf("Resetting view\n");
+                    brot = brot_create(WIDTH, HEIGHT, 255, -2.5, -1.0, 1.0, 1.0);
+                    brot_calculate(brot);
+                    draw_screen(brot, screen);
+                    break;
                 default:
                     break;
                 }
                 break;
 
             case SDL_MOUSEBUTTONDOWN:           //mouse button down
-                printf("Mouse down x:%d, y:%d\n", event.button.x, event.button.y );
+                x1 = event.button.x;
+                y1 = event.button.y;
                 break; 
             case SDL_MOUSEBUTTONUP:           //mouse button up
-                printf("Mouse up x:%d, y:%d\n", event.button.x, event.button.y );
+                x2 = event.button.x;
+                y2 = event.button.y;
+                draw_mandelbrot_section(brot, x1, y1, x2, y2);
+                draw_screen(brot, screen);
                 break; 
 
             }
